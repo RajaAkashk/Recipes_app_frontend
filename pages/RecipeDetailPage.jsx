21797fdd43faad5c08dcefb9bce66b72b7529f06
Raja Akash Kumar <rajaakashk9@gmail.com>
2025-03-29 13:02:41 +0530

    fixed the import error

--- pages/RecipeDetailPage.jsx
+++ pages/RecipeDetailPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import useFetch from "../UseFetch";
+import useFetch from "../useFetch";
 import { useParams } from "react-router-dom";
 
 function RecipeDetailPage() {
